fa22bb7d82e16d2beeb785bbed484abf9d3617d1
TanguyLcr9 <43568780+TanguyLcr9@users.noreply.github.com>
2021-05-27 18:57:32 +0200

    Add Image

---
+++ Test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Test" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="Untitled - 1.png" width="256" height="192"/>
+</tileset>
